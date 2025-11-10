b58f6fc9dc1aae9c4c234b90d609ba5c771d41a9
Federico Ledesma <160787781+DarkFeche@users.noreply.github.com>
2025-11-10 13:08:35 +0000

    Enhance CMD script to log output to report.txt
    Redirect output of repair commands to report.txt for logging.

--- Reparación vía CMD.bat
+++ Reparación vía CMD.bat
@@ -1,13 +1,19 @@
-.
-echo on
-msg /time:3 * "Reparador de Windows iniciado..."
-cd /D %WINDIR%\
-rmdir /s /q SoftwareDistribution
-DISM /Online /Cleanup-Image /CheckHealth
-DISM /Online /Cleanup-Image /ScanHealth
-DISM /Online /Cleanup-Image /RestoreHealth
-DISM /Online /Cleanup-Image /RestoreHealth /Source:"H:\CD" /LimitAccess
-DISM /online /cleanup-image /startcomponentcleanup
-sfc /scannow
-msg * "¡Reparación completada!"
-msg /time:10 * "Si el problema persiste, volver a ejecutar este archivo iniciando la pc en MODO SEGURO CON FUNCIONES DE RED"
+@echo on
+msg * /time:3 "Reparador de Windows iniciado..."
+echo --- Nueva Ejecucion --- >> report.txt
+
+cd /D %WINDIR%
+rmdir /s /q SoftwareDistribution | tee -a report.txt
+
+DISM /Online /Cleanup-Image /CheckHealth | tee -a report.txt
+DISM /Online /Cleanup-Image /ScanHealth | tee -a report.txt
+DISM /Online /Cleanup-Image /RestoreHealth | tee -a report.txt
+DISM /Online /Cleanup-Image /RestoreHealth /Source:"H:\CD" /LimitAccess | tee -a report.txt
+DISM /Online /Cleanup-Image /StartComponentCleanup | tee -a report.txt
+sfc /scannow | tee -a report.txt
+
+msg * /time:2 "Trabajo terminado!"
+msg * /time:10 "Si el problema persiste, volver a ejecutar este archivo iniciando la pc en MODO SEGURO CON FUNCIONES DE RED"
+
+notepad report.txt
+pause
